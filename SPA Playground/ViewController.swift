3b55c926015a57aef17c2212355e366b02026827
Scott Williams <scott.williams@macys.com>
2017-09-17 22:35:40 -0400

    initial commit

--- SPA Playground/ViewController.swift
+++ SPA Playground/ViewController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameInputField: UITextField!
+    @IBOutlet weak var welcomeLabel: UILabel!
+
+    @IBAction func nameEnterButton(_ sender: Any) {
+        self.updateWelcomeLabel();
+    }
+    
+    func updateWelcomeLabel() -> Void {
+        let name = self.nameInputField.text;
+        
+        if (name != "") {
+            self.welcomeLabel.text = "Welcome, " + name! + "!";
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField.text != "") {
+            self.nameInputField.resignFirstResponder();
+            self.updateWelcomeLabel();
+            return true;
+        }
+        return false;
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        self.nameInputField.delegate = self;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
 
